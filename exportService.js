1e31ce1cd6a82fbf40004904d4fb843e23a11ce5
zhangirkhan <zendarol@mail.ru>
2021-11-11 14:32:25 +0600

    First commit

---
+++ exportService.js
@@ -0,0 +1,22 @@
+const xlsx = require('xlsx');
+const path = require('path');
+
+const exportExcel = (data, workSheetColumnNames, workSheetName, filePath) => {
+    const workBook = xlsx.utils.book_new();
+    const workSheetData = [
+        workSheetColumnNames,
+        ... data
+    ];
+    const workSheet = xlsx.utils.aoa_to_sheet(workSheetData);
+    xlsx.utils.book_append_sheet(workBook, workSheet, workSheetName);
+    xlsx.writeFile(workBook, path.resolve(filePath));
+}
+
+const exportReestrsToExcel = (reestrs, workSheetColumnNames, workSheetName, filePath) => {
+    const data = reestrs.map(reestr => {
+        return [reestr.name, reestr.bin_iin, reestr.contacts, reestr.otrasl, reestr.kpved, reestr.production, reestr.nomer_sertificata, reestr.data_vidachi, reestr.sait];
+    });
+    exportExcel(data, workSheetColumnNames, workSheetName, filePath);
+}
+
+module.exports = exportReestrsToExcel;
